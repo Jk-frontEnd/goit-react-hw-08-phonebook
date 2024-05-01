0a84013a3a59d24e8682b3fe5e5e3be45db41936
JulianskyMe <juliakamynskaya@gmail.com>
2024-05-01 18:32:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import ReactDOM from 'react-dom';
-import {App} from './components/App';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter from react-router-dom
 import { Provider } from 'react-redux';
-import {store} from './redux/store';
+import { store } from './redux/store';
+import { App } from './components/App';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter basename="/got-react-hw-08-phonebook">
-  <Provider store={store}>
-    <App />
+    <Provider store={store}>
+      <App />
     </Provider>
-    </BrowserRouter>
+  </BrowserRouter>
 );
