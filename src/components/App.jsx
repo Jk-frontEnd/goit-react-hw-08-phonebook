5b270de0fb907f59efa7acb17cc20393d9260f7e
JulianskyMe <juliakamynskaya@gmail.com>
2024-05-14 21:06:06 +0300

    nope

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Navigation from '../components/Navigation/Navigation';
 import HomePage from '../components/Home/Home';
 import RegisterPage from '../components/RegisterPage/RegisterPage';
 import LoginPage from '../components/LoginPage/LoginPage';
 import ContactsPage from '../components/ContactPage/ContactPage';
+import PrivateRoute from './PrivateRoute/PrivateRoute';
 
 export const App = () => {
   return (
-    <>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/contacts" element={<ContactsPage />} />
-        </Routes>
-      </>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/contacts" element={<PrivateRoute><ContactsPage /></PrivateRoute>} />
+      </Routes>
+    </Router>
   );
 };
--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import { Route, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
+const PrivateRoute = ({ children, redirectTo = '/login' }) => {
   const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
 
-  return (
-    <Route {...rest} render={(props) => (
-      isLoggedIn ? (
-        <Component {...props} />
-      ) : (
-        <Navigate to="/login" />
-      )
-    )} />
-  );
+  return isLoggedIn ? children : <Navigate to={redirectTo} replace />;
 };
 
 export default PrivateRoute;
